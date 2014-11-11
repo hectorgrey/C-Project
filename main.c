4d9d3b3f444dad8f0e8a327cc6c0d5297a69d852
Chris Parker <chp35@aber.ac.uk>
2014-11-11 20:23:25 +0000

    Added main.c to the repository.

---
+++ main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: chris
+ *
+ * Created on 10 November 2014, 14:05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sighting.h"
+
+/*
+ * Takes no arguments.
+ */
+
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
