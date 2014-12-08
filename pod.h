08dcc8c14030f52d0b681b9380c6d44d20239db2
Chris Parker <chp35@aber.ac.uk>
2014-12-08 21:55:14 +0000

    Begun Function 3

--- pod.h
+++ pod.h
@@ -8,7 +8,15 @@
 #ifndef POD_H
 #define	POD_H
 
+typedef struct {
+    individual_list *individuals;
+    int total_mammals;
+} pod;
 
+typedef struct list_pods {
+    pod *content;
+    struct list_pods *next;
+} pod_list;
 
 #endif	/* POD_H */
 
